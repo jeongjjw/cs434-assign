c6413496bd822d4072eb0be87402939b6a9f3e2e
jeongjw <67964247+jeongjjw@users.noreply.github.com>
2021-09-14 12:06:46 +0900

    Implemented recfun (#5)
    * CI workflow setup for recfun
    
    * Implemented pascal triangle and balance
    
    * implemented recfun
    
    * modified java version in PR builder
    
    * Java version fix for PR builder
    
    Co-authored-by: Jaewoo Jeong <jeongjjw@users.noreply.github.com>

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,52 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == r || c == 0) {
+      1
+    } else {
+      pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceAux(balanceNumber: Int, charList: List[Char]): Boolean = {
+      if (balanceNumber < 0) {
+        false
+      } else if (charList.isEmpty) {
+        if (balanceNumber == 0) {
+          true
+        } else {
+          false
+        }
+      } else {
+        if (charList.head == '(') {
+          balanceAux(balanceNumber + 1, charList.tail)
+        } else if (charList.head == ')') {
+          balanceAux(balanceNumber - 1, charList.tail)
+        } else {
+          balanceAux(balanceNumber, charList.tail)
+        }
+      }
+    }
+    balanceAux(0, chars)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) {
+      1
+    } else if (money < 0) {
+      0
+    } else if (coins.isEmpty) {
+      0
+    } else {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
+  }
 }
